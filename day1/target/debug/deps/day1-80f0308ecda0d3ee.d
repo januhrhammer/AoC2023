db272ad448690633b539adc19411509794d9a2d8
Jan Uhrhammer <jan.uhrhammer@gmail.com>
2024-11-10 20:20:10 +0100

    first commit

---
+++ day1/target/debug/deps/day1-80f0308ecda0d3ee.d
@@ -0,0 +1,5 @@
+C:\Users\januh\Documents\AoC2023\day1\target\debug\deps\libday1-80f0308ecda0d3ee.rmeta: src/main.rs
+
+C:\Users\januh\Documents\AoC2023\day1\target\debug\deps\day1-80f0308ecda0d3ee.d: src/main.rs
+
+src/main.rs:
---
+++ day1/target/debug/deps/day1-ba7c66ad98c80a5a.d
@@ -0,0 +1,5 @@
+C:\Users\januh\Documents\AoC2023\day1\target\debug\deps\libday1-ba7c66ad98c80a5a.rmeta: src/main.rs
+
+C:\Users\januh\Documents\AoC2023\day1\target\debug\deps\day1-ba7c66ad98c80a5a.d: src/main.rs
+
+src/main.rs:
